5cb06bbaa0cae71267c15823a6cab1ab20d1bda5
Anastasiia Petrochenkova <a.petrochenkova@ibolit.pro>
2022-02-25 11:45:26 +0300

    Задание 4 1. Реализованы функции для создания фикстур. 2. Удалены тестовые классы. 3. Сделаны тесты обычными функциями, в которые фикстуры передаются в качестве параметра.

---
+++ application.py
@@ -0,0 +1,75 @@
+# -*- coding: utf-8 -*-
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+
+class Application:
+
+    def __init__(self, method):
+        self.driver = webdriver.Chrome(executable_path="/Users/anastasiia/PycharmProjects/python_training/chromedriver")
+
+    def open_home_page(self):
+        self.driver.get("http://localhost/addressbook/index.php")
+
+    def login(self, user_name, password):
+        self.open_home_page()
+        self.driver.find_element(By.NAME, "user").click()
+        self.driver.find_element(By.NAME, "user").clear()
+        self.driver.find_element(By.NAME, "user").send_keys(user_name)
+        self.driver.find_element(By.NAME, "pass").click()
+        self.driver.find_element(By.NAME, "pass").clear()
+        self.driver.find_element(By.NAME, "pass").send_keys(password)
+        self.driver.find_element(By.CSS_SELECTOR, "input:nth-child(7)").click()
+
+    def open_groups_page(self):
+        self.driver.find_element(By.LINK_TEXT, "groups").click()
+
+    def create_group(self, group):
+        # open groups page
+        self.open_groups_page()
+        # init group creation
+        self.driver.find_element(By.NAME, "new").click()
+         # fill group form
+        self.driver.find_element(By.NAME, "group_name").click()
+        self.driver.find_element(By.NAME, "group_name").send_keys(group.name)
+        self.driver.find_element(By.NAME, "group_header").click()
+        self.driver.find_element(By.NAME, "group_header").send_keys(group.header)
+        self.driver.find_element(By.NAME, "group_footer").click()
+        self.driver.find_element(By.NAME, "group_footer").send_keys(group.footer)
+        # submit group creation
+        self.driver.find_element(By.NAME, "submit").click()
+        # return to groups page
+        self.return_to_groups_page()
+
+    def return_to_groups_page(self):
+        self.driver.find_element(By.LINK_TEXT, "group page").click()
+
+    def logout(self):
+        self.driver.find_element(By.LINK_TEXT, "Logout").click()
+
+    def add_contact(self, contact):
+        self.driver.find_element(By.LINK_TEXT, "add new").click()
+        self.driver.find_element(By.NAME, "firstname").click()
+        self.driver.find_element(By.NAME, "firstname").send_keys(contact.firstname)
+        self.driver.find_element(By.NAME, "middlename").click()
+        self.driver.find_element(By.NAME, "middlename").send_keys(contact.middlename)
+        self.driver.find_element(By.NAME, "lastname").click()
+        self.driver.find_element(By.NAME, "lastname").send_keys(contact.lastname)
+        self.driver.find_element(By.NAME, "nickname").click()
+        self.driver.find_element(By.NAME, "nickname").send_keys(contact.nickname)
+        self.driver.find_element(By.NAME, "title").click()
+        self.driver.find_element(By.NAME, "title").send_keys(contact.title)
+        self.driver.find_element(By.NAME, "company").click()
+        self.driver.find_element(By.NAME, "company").send_keys(contact.company)
+        self.driver.find_element(By.NAME, "address").click()
+        self.driver.find_element(By.NAME, "address").send_keys(contact.address)
+        self.driver.find_element(By.NAME, "theform").click()
+        self.driver.find_element(By.NAME, "home").click()
+        self.driver.find_element(By.NAME, "home").send_keys(contact.home_phone)
+        self.driver.find_element(By.NAME, "mobile").click()
+        self.driver.find_element(By.NAME, "mobile").send_keys(contact.mobile_phone)
+        self.driver.find_element(By.NAME, "email").click()
+        self.driver.find_element(By.NAME, "email").send_keys(contact.email)
+        self.driver.find_element(By.CSS_SELECTOR, "input:nth-child(87)").click()
+
+    def destroy(self):
+        self.driver.quit()
--- test_add_group.py
+++ test_add_group.py
@@ -1,62 +1,22 @@
 # -*- coding: utf-8 -*-
-from selenium import webdriver
-from selenium.webdriver.common.by import By
 from group import Group
+from application import Application
+import pytest
 
-class Test_add_group():
-  def setup_method(self, method):
-    self.driver = webdriver.Chrome(executable_path="/Users/anastasiia/PycharmProjects/python_training/chromedriver")
+@pytest.fixture
+def app(request):
+  fixture = Application(request)
+  request.addfinalizer(fixture.destroy)
+  return fixture
 
-  def open_home_page(self):
-    self.driver.get("http://localhost/addressbook/index.php")
+def test_add_new_group(app):
+  app.login(user_name='admin', password='secret')
+  app.create_group(Group(name="имя", header="хедер", footer="футер"))
+  app.logout()
 
-  def login(self, user_name, password):
-    self.driver.find_element(By.NAME, "user").click()
-    self.driver.find_element(By.NAME, "user").clear()
-    self.driver.find_element(By.NAME, "user").send_keys(user_name)
-    self.driver.find_element(By.NAME, "pass").click()
-    self.driver.find_element(By.NAME, "pass").clear()
-    self.driver.find_element(By.NAME, "pass").send_keys(password)
-    self.driver.find_element(By.CSS_SELECTOR, "input:nth-child(7)").click()
+def test_add_empty_group(app):
+  app.login(user_name='admin', password='secret')
+  app.create_group(Group(name="", header="", footer=""))
+  app.logout()
 
-  def open_groups_page(self):
-    self.driver.find_element(By.LINK_TEXT, "groups").click()
 
-  def create_group(self, group):
-    # init group creation
-    self.driver.find_element(By.NAME, "new").click()
-    # fill group form
-    self.driver.find_element(By.NAME, "group_name").click()
-    self.driver.find_element(By.NAME, "group_name").send_keys(group.name)
-    self.driver.find_element(By.NAME, "group_header").click()
-    self.driver.find_element(By.NAME, "group_header").send_keys(group.header)
-    self.driver.find_element(By.NAME, "group_footer").click()
-    self.driver.find_element(By.NAME, "group_footer").send_keys(group.footer)
-    # submit group creation
-    self.driver.find_element(By.NAME, "submit").click()
-
-  def return_to_groups_page(self):
-    self.driver.find_element(By.LINK_TEXT, "group page").click()
-
-  def logout(self):
-    self.driver.find_element(By.LINK_TEXT, "Logout").click()
-
-  def test_add_new_group(self):
-    self.open_home_page()
-    self.login(user_name='admin', password='secret')
-    self.open_groups_page()
-    self.create_group(Group(name="имя", header="хедер", footer="футер"))
-    self.return_to_groups_page()
-    self.logout()
-
-  def test_add_empty_group(self):
-    self.open_home_page()
-    self.login(user_name='admin', password='secret')
-    self.open_groups_page()
-    self.create_group(Group(name="", header="", footer=""))
-    self.return_to_groups_page()
-    self.logout()
-
-  def teardown_method(self, method):
-    self.driver.quit()
-  
