4dc5c2ac30cea91d5950962d5df1c2a48af29353
Anastasiia Petrochenkova <a.petrochenkova@ibolit.pro>
2022-02-28 12:03:15 +0300

    Добавлен тест на редактирование первого контакта.

---
+++ test/test_edit_contact.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+from model.contact import Contact
+
+
+def test_edit_contact(app):
+    app.session.login(user_name='admin', password='secret')
+    app.contact.edit_first_contact(
+        Contact(firstname="Имя3", middlename="Отчество3", lastname="Фамилия3", nickname="Никнейм3", title="Название3",
+                company="Компания3", address="Адрес3", home_phone="7495123123113", mobile_phone="7977881489913",
+                email="test3@test.test"))
+    app.session.logout()
