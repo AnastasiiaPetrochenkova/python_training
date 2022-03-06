835d9a9ba5d0f7bcc3a6d544f77f30b5424abd4e
Anastasiia Petrochenkova <a.petrochenkova@ibolit.pro>
2022-03-06 11:39:22 +0300

    Реализована проверка валидности фикстуры и проверка состояния сессии

--- fixture/session.py
+++ fixture/session.py
@@ -7,7 +7,6 @@ class SessionHelper:
         self.app = app
         self.driver = self.app.driver
 
-
     def login(self, user_name, password):
         self.app.open_home_page()
         self.driver.find_element(By.NAME, "user").click()
@@ -20,3 +19,21 @@ class SessionHelper:
 
     def logout(self):
         self.driver.find_element(By.LINK_TEXT, "Logout").click()
+
+    def is_logged_in(self):
+        return len(self.driver.find_elements(By.LINK_TEXT, "Logout")) > 0
+
+    def is_logged_in_as(self, username):
+        return self.driver.find_element(By.XPATH, '//*[@id="top"]/form/b').text == "(" + username + ")"
+
+    def ensure_logout(self):
+        if self.is_logged_in():
+            self.logout()
+
+    def ensure_login(self, username, password):
+        if self.is_logged_in():
+            if self.is_logged_in_as(username):
+                return
+            else:
+                self.logout()
+        self.login(username, password)
