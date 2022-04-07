0c91e40ffdde2ea496b98d63dcddfc1d1eaa2d1e
Anastasiia Petrochenkova <a.petrochenkova@ibolit.pro>
2022-04-07 10:37:25 +0300

    Задание 21

--- test/test_check_data.py
+++ test/test_check_data.py
@@ -1,41 +1,46 @@
 import re
 from random import randrange
+from fixture.contact import Contact
 
 
-def test_alldata_on_home_page(app):
-    alldata_from_home_page = app.contact.get_contact_list()
-    index = randrange(len(alldata_from_home_page))
-    person_data_from_home_page = alldata_from_home_page[index]
-    person_data_from_edit_page = app.contact.get_contact_info_from_edit_page(index)
-    assert person_data_from_home_page.firstname == person_data_from_edit_page.firstname.strip()
-    assert person_data_from_home_page.lastname == person_data_from_edit_page.lastname.strip()
-    assert person_data_from_home_page.address == person_data_from_edit_page.address.strip()
-    assert person_data_from_home_page.id == person_data_from_edit_page.id
-    assert person_data_from_home_page.all_phones_from_home_page == merge_phones_like_on_home_page(person_data_from_edit_page)
-    assert person_data_from_home_page.all_emails_from_home_page == merge_emails_like_on_home_page(person_data_from_edit_page)
+def test_alldata_on_home_page(app, db):
+    contacts_from_home_page = app.contact.get_contact_list()
+    contacts_from_db = db.get_contact_list()
+    assert sorted(contacts_from_home_page, key=Contact.id_or_max) == sorted(contacts_from_db, key=Contact.id_or_max)
 
-
-def test_phones_on_contact_view_page(app):
-    contact_from_view_page = app.contact.get_contact_from_view_page(0)
-    contact_from_edit_page = app.contact.get_contact_info_from_edit_page(0)
-    assert contact_from_view_page.home_phone == contact_from_edit_page.home_phone
-    assert contact_from_view_page.work_phone == contact_from_edit_page.work_phone
-    assert contact_from_view_page.mobile_phone == contact_from_edit_page.mobile_phone
-    assert contact_from_view_page.secondary_phone == contact_from_edit_page.secondary_phone
-
-
-def clear(s):
-    return re.sub("[() -]", "", s)
-
-
-def merge_phones_like_on_home_page(contact):
-    return "\n".join(filter(lambda x: x != "",
-                            map(lambda x: clear(x),
-                                filter(lambda x: x is not None,
-                                       [contact.home_phone, contact.mobile_phone, contact.work_phone, contact.secondary_phone]))))
-
-
-def merge_emails_like_on_home_page(contact):
-    return "\n".join(filter(lambda x: x != "",
-                            filter(lambda x: x is not None,
-                                   [contact.email1, contact.email2, contact.email3])))
+#     alldata_from_home_page = app.contact.get_contact_list()
+#     index = randrange(len(alldata_from_home_page))
+#     person_data_from_home_page = alldata_from_home_page[index]
+#     person_data_from_edit_page = app.contact.get_contact_info_from_edit_page(index)
+#     assert person_data_from_home_page.firstname == person_data_from_edit_page.firstname.strip()
+#     assert person_data_from_home_page.lastname == person_data_from_edit_page.lastname.strip()
+#     assert person_data_from_home_page.address == person_data_from_edit_page.address.strip()
+#     assert person_data_from_home_page.id == person_data_from_edit_page.id
+#     assert person_data_from_home_page.all_phones_from_home_page == merge_phones_like_on_home_page(person_data_from_edit_page)
+#     assert person_data_from_home_page.all_emails_from_home_page == merge_emails_like_on_home_page(person_data_from_edit_page)
+#
+#
+# def test_phones_on_contact_view_page(app):
+#     contact_from_view_page = app.contact.get_contact_from_view_page(0)
+#     contact_from_edit_page = app.contact.get_contact_info_from_edit_page(0)
+#     assert contact_from_view_page.home_phone == contact_from_edit_page.home_phone
+#     assert contact_from_view_page.work_phone == contact_from_edit_page.work_phone
+#     assert contact_from_view_page.mobile_phone == contact_from_edit_page.mobile_phone
+#     assert contact_from_view_page.secondary_phone == contact_from_edit_page.secondary_phone
+#
+#
+# def clear(s):
+#     return re.sub("[() -]", "", s)
+#
+#
+# def merge_phones_like_on_home_page(contact):
+#     return "\n".join(filter(lambda x: x != "",
+#                             map(lambda x: clear(x),
+#                                 filter(lambda x: x is not None,
+#                                        [contact.home_phone, contact.mobile_phone, contact.work_phone, contact.secondary_phone]))))
+#
+#
+# def merge_emails_like_on_home_page(contact):
+#     return "\n".join(filter(lambda x: x != "",
+#                             filter(lambda x: x is not None,
+#                                    [contact.email1, contact.email2, contact.email3])))
